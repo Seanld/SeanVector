bcbfbc2914016a655b6647a4384b6aede5f33da0
Sean Wilkerson <seanldmcdonald@gmail.com>
2018-01-18 10:32:43 -0700

    Out-of-bounds exception, and addressed memory leaks.

--- SeanVector.cpp
+++ SeanVector.cpp
@@ -1,44 +1,64 @@
-#include "SeanVector.h"
+#include "MyVector.h"
 
-SeanVector::SeanVector() {
-    int CAPACITY = 2;
-    int ARROW = 0; // The index at which the next `push_back` value will be stored. Incremented every call.
-    
-    int* svArrayPtr = new int[CAPACITY];
+MyVector::MyVector() {
+	CAPACITY = 2;
+	ARROW = 0; // The index at which the next `push_back` value will be stored. Incremented every call.
+
+	svArrayPtr = new int[CAPACITY];
+	svArrayPtrBackup = nullptr;
 }
 
-SeanVector::SeanVector(int cap) {
-    int* svArrayPtr = new int[cap];
-	
-	int CAPACITY = cap;
-	int ARROW = 0;
+MyVector::MyVector(int cap) {
+	svArrayPtr = new int[cap];
+
+	CAPACITY = cap;
+	ARROW = 0;
 }
 
-int SeanVector::size() {
-    // How would we know where the values actually stop, and the following zeroes in the array aren't actually values that are supposed to be part of the array?
+int MyVector::size() {
+	return ARROW + 1;
 }
 
-int SeanVector::capacity() {
-    return CAPACITY;
+int MyVector::capacity() {
+	return CAPACITY;
 }
 
-int SeanVector::clear() {
-    CAPACITY = 2; // Reset total capacity.
-    
-    delete[] svArrayPtr; // Dispose of the old array.
-    
-    svArrayPtr = new int[CAPACITY]; // Allocate new array.
+void MyVector::clear() {
+	CAPACITY = 2; // Reset total capacity.
+	ARROW = 0;
+
+	delete[] svArrayPtr; // Dispose of the old array.
+
+	svArrayPtr = new int[CAPACITY]; // Allocate new array.
 }
 
-void SeanVector::push_back(int v) {
-    svArrayPtr[ARROW] = v;
-    ARROW++;
+void MyVector::push_back(int v) {
+	if ((ARROW + 1) >= CAPACITY) { // We need to expand the array.
+		CAPACITY *= 2;
+		svArrayPtrBackup = new int[CAPACITY];
+
+		for (int i = 0; i < (CAPACITY / 2); i++) { // Copy contents of original vector over to the new one.
+			svArrayPtrBackup[i] = svArrayPtr[i];
+		}
+
+		delete[] svArrayPtr; // Delete original data from memory, then...
+		svArrayPtr = svArrayPtrBackup;
+		svArrayPtrBackup = nullptr; // Move the pointer back to original pointer, and reset backup one.
+	}
+
+	svArrayPtr[ARROW] = v;
+	ARROW++;
 }
 
-int SeanVector::at(int i) {
-    return svArrayPtr[i];
+int MyVector::at(int i) {
+	if (i >= (CAPACITY-1)) {
+		throw i;
+	}
+	else {
+		return svArrayPtr[i];
+	}
 }
 
-SeanVector::~SeanVector() {
+MyVector::~MyVector() {
 	delete[] svArrayPtr;
 }
