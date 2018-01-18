4feb80355f655710f1092146634c50b1d635b844
Sean Wilkerson <seanldmcdonald@gmail.com>
2018-01-18 09:30:03 -0700

    Update SeanVector.cpp

--- SeanVector.cpp
+++ SeanVector.cpp
@@ -1,9 +1,44 @@
 #include "SeanVector.h"
 
-SeanVector::SeanVector(int size) {
-	int* svArray = new int[size];
+SeanVector::SeanVector() {
+    int CAPACITY = 2;
+    int ARROW = 0; // The index at which the next `push_back` value will be stored. Incremented every call.
+    
+    int* svArrayPtr = new int[CAPACITY];
+}
+
+SeanVector::SeanVector(int cap) {
+    int* svArrayPtr = new int[cap];
+	
+	int CAPACITY = cap;
+	int ARROW = 0;
+}
+
+int SeanVector::size() {
+    // How would we know where the values actually stop, and the following zeroes in the array aren't actually values that are supposed to be part of the array?
+}
+
+int SeanVector::capacity() {
+    return CAPACITY;
+}
+
+int SeanVector::clear() {
+    CAPACITY = 2; // Reset total capacity.
+    
+    delete[] svArrayPtr; // Dispose of the old array.
+    
+    svArrayPtr = new int[CAPACITY]; // Allocate new array.
+}
+
+void SeanVector::push_back(int v) {
+    svArrayPtr[ARROW] = v;
+    ARROW++;
+}
+
+int SeanVector::at(int i) {
+    return svArrayPtr[i];
 }
 
 SeanVector::~SeanVector() {
-	
+	delete[] svArrayPtr;
 }
